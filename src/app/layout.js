443c401d285abf3c220263e33056655289e21e9a
Andriana-Petrenko <144230812+Andriana-Petrenko@users.noreply.github.com>
2025-05-14 00:46:30 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import { Montserrat } from 'next/font/google';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
